c0777cd4a7877444e2c4aaa8d1103a8cccae4afd
Emma Nielsen <Emmanielsen843@gmail.com>
2023-09-20 12:20:26 +0200

    detailsinfo changes

--- src/Components/Pages/Sortering/SorteringDetails/DetailsInfo/DetailsInfo.jsx
+++ src/Components/Pages/Sortering/SorteringDetails/DetailsInfo/DetailsInfo.jsx
@@ -1,30 +1,30 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const DetailsInfo = () => {
-  const [categories, setCategories] = useState([]);
-  // const { section_id } = useParams();
+  const [category, setcategory] = useState({});
+  const { section_id } = useParams();
 
   useEffect(() => {
     const getData = async () => {
       try {
         const result = await axios.get(
-          `http://localhost:4000/section?incl_categories=true&incl_types=true`
+          `http://localhost:4000/section/${section_id}`
         );
-        setCategories(result.data[0].categories); // Access categories from the first item in the array
+        setcategory(result.data); 
       } catch (err) {
         console.error(err);
       }
     };
     getData();
-  }, [setCategories]);
+  }, [section_id]);
 
   return (
     <div>
-      {categories && categories.map((item) => (
-        <p key={item.title}>{item.title}</p>
-      ))}
+      {category ? (
+        <p>{category.title}</p>
+      ) : null}
     </div>
   );
 };
